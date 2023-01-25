fe5e15e33f449a065e2669ac998e5d70fb207c86
unknown <solja484@gmail.com>
2023-01-25 17:15:14 +0200

    updated demo page description

--- vue2/typescript/src/views/VueFlexmonsterExamples/PivotTableDemo.vue
+++ vue2/typescript/src/views/VueFlexmonsterExamples/PivotTableDemo.vue
@@ -6,6 +6,21 @@
         Flexmonster is a fast and powerful JavaScript pivot grid for data
         visualization and reporting.
       </p>
+      <p>
+        With Flexmonster, you can create reports based on many data sources,
+        including JSON, CSV, MongoDB, and SQL databases. Our component is easy
+        to customize and configure, so it can be seamlessly integrated into any
+        project.
+      </p>
+      <p>
+        Visit
+        <a
+            href="https://www.flexmonster.com/doc/?r=rm_vue"
+            target="_blank"
+            class="title-link"
+        >our documentation</a>
+        for step-by-step guidance on Flexmonster.
+      </p>
     </div>
     <Pivot
       ref="pivot"
@@ -18,24 +33,6 @@
       v-bind:beforetoolbarcreated="customizeToolbar"
       _v-bind:licenseKey="'XXXX-XXXX-XXXX-XXXX-XXXX'"
     ></Pivot>
-    <div class="description-blocks">
-      <p>
-        With Flexmonster, you can create reports based on many data sources,
-        including JSON, CSV, MongoDB, and SQL databases. Our component is easy
-        to customize and configure, so it can be seamlessly integrated into any
-        project.
-      </p>
-      <p>
-        Visit
-        <a
-          href="https://www.flexmonster.com/doc/?r=rm_vue"
-          target="_blank"
-          class="title-link"
-          >our documentation</a
-        >
-        for step-by-step guidance on Flexmonster.
-      </p>
-    </div>
   </div>
 </template>
 
--- vue3/ES6/src/views/VueFlexmonsterExamples/PivotTableDemo.vue
+++ vue3/ES6/src/views/VueFlexmonsterExamples/PivotTableDemo.vue
@@ -6,6 +6,21 @@
         Flexmonster is a fast and powerful JavaScript pivot grid for data
         visualization and reporting.
       </p>
+      <p>
+        With Flexmonster, you can create reports based on many data sources,
+        including JSON, CSV, MongoDB, and SQL databases. Our component is easy
+        to customize and configure, so it can be seamlessly integrated into any
+        project.
+      </p>
+      <p>
+        Visit
+        <a
+            href="https://www.flexmonster.com/doc/?r=rm_vue"
+            target="_blank"
+            class="title-link"
+        >our documentation</a>
+        for step-by-step guidance on Flexmonster.
+      </p>
     </div>
     <Pivot
       toolbar
@@ -17,24 +32,6 @@
       v-bind:beforetoolbarcreated="customizeToolbar"
       _v-bind:licenseKey="'XXXX-XXXX-XXXX-XXXX-XXXX'"
     />
-    <div class="description-blocks">
-      <p>
-        With Flexmonster, you can create reports based on many data sources,
-        including JSON, CSV, MongoDB, and SQL databases. Our component is easy
-        to customize and configure, so it can be seamlessly integrated into any
-        project.
-      </p>
-      <p>
-        Visit
-        <a
-          href="https://www.flexmonster.com/doc/?r=rm_vue"
-          target="_blank"
-          class="title-link"
-          >our documentation</a
-        >
-        for step-by-step guidance on Flexmonster.
-      </p>
-    </div>
   </div>
 </template>
 
