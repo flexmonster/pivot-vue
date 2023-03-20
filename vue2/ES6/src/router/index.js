546e996727211f7513b76dc13324870ca317d387
Solomiia Andrusiv <solja@flexmonster.com>
2023-03-20 13:42:36 +0200

    modified index.js

--- vue2/ES6/src/router/index.js
+++ vue2/ES6/src/router/index.js
@@ -3,54 +3,57 @@ import PivotTableDemo from '../views/VueFlexmonsterExamples/PivotTableDemo.vue'
 //import UsingAPICalls from '../views/VueFlexmonsterExamples/UsingAPICalls.vue'
 
 const router = new VueRouter({
-    mode: "history",
-
-    routes: [{
-        path: '/pivot-table-demo',
-        name: 'PivotTableDemo',
-        component: PivotTableDemo
-    },
-        {
-            path: '/handling-events',
-            name: 'HandlingEvents',
-            component: () => import('@/views/VueFlexmonsterExamples/HandlingEvents.vue')
-        },
-        {
-            path: '/using-api-calls',
-            name: 'UsingAPICalls',
-            component: () => import('@/views/VueFlexmonsterExamples/UsingAPICalls.vue')
-        },
-        {
-            path: '/updating-data',
-            name: 'UpdatingData',
-            component: () => import('@/views/VueFlexmonsterExamples/UpdatingData.vue')
-        },
-        {
-            path: '/customizing-toolbar',
-            name: 'CustomizingToolbar',
-            component: () => import('@/views/VueFlexmonsterExamples/CustomizingToolbar.vue')
-        },
-        {
-            path: '/customizing-grid',
-            name: 'CustomizingGrid',
-            component: () => import('@/views/VueFlexmonsterExamples/CustomizingGrid.vue')
-        },
-        {
-            path: '/with-highcharts',
-            name: 'WithHighcharts',
-            component: () => import('@/views/VueFlexmonsterExamples/WithHighcharts.vue')
-        },
-        {
-            path: '/with-amcharts',
-            name: 'WithAmcharts',
-            component: () => import('@/views/VueFlexmonsterExamples/WithAmcharts.vue')
-        },
-        {
-            path: '/:pathMatch(.*)*',
-            name: 'Home',
-            component: PivotTableDemo
-        }
-    ]
+  routes: [{
+    path: '/pivot-table-demo',
+    name: 'PivotTableDemo',
+    component: PivotTableDemo
+  },
+  {
+    path: '/handling-events',
+    name: 'HandlingEvents',
+    component: () => import('@/views/VueFlexmonsterExamples/HandlingEvents.vue')
+  },
+  {
+    path: '/using-api-calls',
+    name: 'UsingAPICalls',
+    component: () => import('@/views/VueFlexmonsterExamples/UsingAPICalls.vue')
+  },
+  {
+    path: '/updating-data',
+    name: 'UpdatingData',
+    component: () => import('@/views/VueFlexmonsterExamples/UpdatingData.vue')
+  },
+  {
+    path: '/customizing-toolbar',
+    name: 'CustomizingToolbar',
+    component: () => import('@/views/VueFlexmonsterExamples/CustomizingToolbar.vue')
+  },
+  {
+    path: '/customizing-grid',
+    name: 'CustomizingGrid',
+    component: () => import('@/views/VueFlexmonsterExamples/CustomizingGrid.vue')
+  },
+  {
+    path: '/with-highcharts',
+    name: 'WithHighcharts',
+    component: () => import('@/views/VueFlexmonsterExamples/WithHighcharts.vue')
+  },
+  {
+    path: '/with-amcharts4',
+    name: 'WithAmcharts4',
+    component: () => import('@/views/VueFlexmonsterExamples/WithAmcharts4.vue')
+  },
+  {
+    path: '/with-amcharts',
+    name: 'WithAmcharts',
+    component: () => import('@/views/VueFlexmonsterExamples/WithAmcharts.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'Home',
+    component: PivotTableDemo
+  }
+  ]
 })
 
 export default router
